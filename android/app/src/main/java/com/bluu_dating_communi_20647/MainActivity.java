e4332c120296e531b246aaead2cdd72244054342
Crowdbotics <team@crowdbotics.com>
2020-09-24 19:56:57 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/bluu_dating_communi_20647/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bluu_dating_communi_20647;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "bluu_dating_communi_20647";
+    }
+}
